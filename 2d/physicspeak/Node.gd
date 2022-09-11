267d32f33db5b9f51cb8936dd06c214c4dc8a326
David <david@Air-von-David.lan>
2022-09-11 19:08:30 +0200

    convert to godot 3.5; add fpslayer to 3d

--- 2d/physicspeak/Node.gd
+++ 2d/physicspeak/Node.gd
@@ -2,14 +2,8 @@ extends Node
 
 
 var count = 0
-var score = 0
-var runningTime = 8 # in seconds
-var time = 0
 
 var sprite = preload("res://Sprite.tscn")
-onready var label = get_node("CanvasLayer/Panel/Label")
-onready var label2 = get_node("CanvasLayer/Panel/Label1")
-onready var label3 = get_node("CanvasLayer/Panel/Label2")
 onready var container = get_node("sprites")
 
 var sprites = []
@@ -18,19 +12,8 @@ var active = false
 func _ready():
 	while count < 2000:
 		spawn_sprite()
-	set_process(true)
 	pass
 
-func _process(delta):
-	time = time + delta
-	score = score + Engine.get_frames_per_second()
-	label2.set_text(String(Engine.get_frames_per_second()))
-	label3.set_text(String(score))
-	if(time > 8):
-		print(score)
-		get_tree().quit()
-	pass
-	
 func spawn_sprite():
 	count = count + 1
 	var x = randf() * OS.get_window_size().x #+ OS.get_window_size().x
@@ -40,6 +23,4 @@ func spawn_sprite():
 	newSprite.set_position(Vector2(x, y))
 	container.add_child(newSprite)
 	sprites.append(newSprite)
-	
-	label.text = String(count)
 	pass
